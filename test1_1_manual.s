91f892c018a8457e8c0b8c6097ec4fbea130b691
mengning <mengning@ustc.edu.cn>
2019-04-23 18:19:50 +0800

    create a assemble file manually

--- test1_1_manual.s
+++ test1_1_manual.s
@@ -1,20 +1,3 @@
-int g(int x)
-{
-    return x + 3;
-}
-
-int f(int x)
-{
-    return g(x);
-}
-
-int main(void)
-{
-    return f(8) + 1;
-}
-
-// assemble
-
 g:
     pushl   %ebp
     movl    %esp,%ebp
